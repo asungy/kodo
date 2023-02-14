dbfe0162967ada6d78ec0edac6f344156594bac3
Alek Sung <aleksungis@gmail.com>
2023-02-14 01:29:09 -0500

    Removed unused code

--- runtime/src/vm/thread/mod.rs
+++ runtime/src/vm/thread/mod.rs
@@ -1,17 +1,1 @@
-mod frame;
-pub mod data_stack;
-
-/// Represents a thread in the virtual machine.
-pub struct Thread {
-
-}
-
-impl Thread {
-    pub fn new(inst: Vec<u8>) -> Self {
-        todo!("Implement me!");
-    }
-
-    pub fn start(&mut self) -> Self {
-        todo!("Implement me!");
-    }
-}
+pub(super) mod data_stack;
